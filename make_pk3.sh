046fb772253da6f64365542a49d9d1ba981323af
Eliot Lash <fadookie@gmail.com>
2025-07-06 06:44:30 -0700

    add script for making windows portable standalone build

--- make_pk3.sh
+++ make_pk3.sh
@@ -4,9 +4,12 @@ set -o nounset
 set -o pipefail
 
 GIT_BUILD_NAME=$(echo -n `date "+%Y%m%d_%H%M"`_`git rev-parse --short HEAD`)
-ARCHIVE_NAME="raw/builds/mutant-ages-dating-sim-2_${GIT_BUILD_NAME}.pk3"
+ARCHIVE_NAME="mutant-ages-dating-sim-2.pk3"
+#ARCHIVE_NAME="raw/builds/mutant-ages-dating-sim-2_${GIT_BUILD_NAME}.pk3"
 
 set -o xtrace
 # Set flag --worktree-attributes to use .gitattributes from working directory instead of HEAD commit
 git archive --format=zip -o "$ARCHIVE_NAME" HEAD:src
-unzip -l "$ARCHIVE_NAME" | less
+
+# Comment out to not display archive contents
+# unzip -l "$ARCHIVE_NAME" | less
