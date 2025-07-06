16afcd8a7f25471e788ea2043f12642a4bcd0ed7
Eliot Lash <fadookie@gmail.com>
2025-07-06 06:23:17 -0700

    add script to make pk3 archive from git archive

---
+++ make_pk3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GIT_BUILD_NAME=$(echo -n `date "+%Y%m%d_%H%M"`_`git rev-parse --short HEAD`)
+ARCHIVE_NAME="raw/builds/mutant-ages-dating-sim-2_${GIT_BUILD_NAME}.pk3"
+
+set -o xtrace
+# Set flag --worktree-attributes to use .gitattributes from working directory instead of HEAD commit
+git archive --format=zip -o "$ARCHIVE_NAME" HEAD:src
+unzip -l "$ARCHIVE_NAME" | less
