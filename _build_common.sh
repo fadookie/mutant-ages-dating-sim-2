5b028f35752f4023e2d415cd733ac1fe5a55fe24
Eliot Lash <fadookie@gmail.com>
2025-08-16 08:50:16 -0700

    update windows standalone wine bottle path again

--- _build_common.sh
+++ _build_common.sh
@@ -3,5 +3,5 @@ PK3_ARCHIVE_INPUT_NAME="mutant-ages-dating-sim-2.pk3"
 ARCHIVE_BASENAME="mutant-ages-dating-sim-2_${GIT_BUILD_NAME}"
 PK3_ARCHIVE_COPY_PATH="${SCRIPT_DIR}/raw/builds/${ARCHIVE_BASENAME}.pk3"
 WIN_ARCHIVE_PATH="${SCRIPT_DIR}/raw/builds/${ARCHIVE_BASENAME}_windows_standalone.zip"
-WIN_STANDALONE_PARENT_PATH="$HOME/Library/Containers/com.isaacmarovitz.Whisky/Bottles/97677CB0-64BD-410F-9730-91A19AB230B9/drive_c/Program Files" # This path is specific to MacBook Pro 2024
+WIN_STANDALONE_PARENT_PATH="$HOME/Library/Containers/com.isaacmarovitz.Whisky/Bottles/97677CB0-64BD-410F-9730-91A19AB230B9/drive_c/users/crossover/Desktop" # This path is specific to MacBook Pro 2024
 WIN_STANDALONE_RELATIVE_PATH="gzdoom-4-14-2-windows"
